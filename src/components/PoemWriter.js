5b3916e80a478729e572e8e77f18c6b2cd8d49a0
deanban <pbp2005@gmail.com>
2017-09-18 21:11:53 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -2,27 +2,97 @@ import React from 'react';
 
 class PoemWriter extends React.Component {
   constructor() {
-    super();
+    super()
 
-    this.state = {};
+    this.state = {
+      value: '',
+      poemText: []
+    }
   }
 
+  handleChange = event => {
+    this.setState({
+      value: event.target.value,
+      poemText: event.target.value   
+    }) 
+    
+    
+  }
+
+  valid() {
+    var lines = this.state.value.split("\n")
+    
+    if (lines[0]) {
+      
+      var line1 = lines[0].trim()
+      var words1 = line1.split(' ').length  
+          
+    }
+    if (lines[1]) {
+      var line2 = lines[1].trim()
+      var words2 = line2.split(' ').length  
+          
+    }
+    if (lines[2]) {
+      var line3 = lines[2].trim()
+      var words3 = line3.split(' ').length  
+          
+    }
+    
+    console.log('lines.length', lines.length)
+
+    if (lines.length === 3 && (words1 ===5 && words2 === 3 && words3 === 5)) {
+      return true
+    
+    } else if (this.state.value === '') {
+        return false
+    
+    } else {
+      return false
+    
+    }
+    }
+        
+    
+    
+  
+
   render() {
-    return (
-      <div>
+    if (this.valid()) {
+       return (     
+        <div>
         <textarea 
           rows="3" 
-          cols="60" 
+          cols="60"      
+          id="texta"     
+          name="textarea"
+          value={this.state.value}
+          onChange={this.handleChange}
         />
-        <div 
-          id="poem-validation-error" 
-          style={{color: 'red'}}
-        >
+        </div>
+      )
+    } else {
+      return (
+        <div>
+        <textarea 
+          rows="3" 
+          cols="60"      
+          id="texta"     
+          name="textarea"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+
+        <div id="poem-validation-error" style={{color: 'red'}}>
           This poem is not written in the right structure!
         </div>
       </div>
-    );
+        )
+    }
+    
   }
 }
 
-export default PoemWriter;
+
+
+export default PoemWriter
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {val: ''};
+  }
+
+  handleChange = event => {
+    this.setState({val: event.target.value})
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.val} onChange={this.handleChange} />
+        <p>Characters remaining: {this.props.maxChars - this.state.val.length}</p>
       </div>
     );
   }
